847cdd44700e0673f876cccfc1e06353425f9c9f
GreaseMonkey <thematrixeatsyou+s1sms@gmail.com>
2026-01-30 14:28:14 +1300

    Prepare to use special slot mappings

--- tools/dislib/annotator.py
+++ tools/dislib/annotator.py
@@ -86,6 +86,13 @@ class Annotator:
         addr = parse_addr(addr_str)
         self.rom.forced_immediates.add(self.rom.virt_to_phys(addr))
 
+    def _annotcmd_banksetting(self, slot_idx_str: str, bank_idx_str: str, start_addr_str: str, end_addr_str: str) -> None:
+        slot_idx = int(slot_idx_str)
+        bank_idx = int(bank_idx_str, 16)
+        start_addr = parse_addr(start_addr_str)
+        end_addr = parse_addr(end_addr_str)
+        print(f"TODO annotate slot {slot_idx:d} to be bank {bank_idx:02X} for opcodes {start_addr[0]:02X}:{start_addr[1]:04X} until {end_addr[0]:02X}:{end_addr[1]:04X}")
+
     def annot_set_addr_type(
         self, virt_addr: VirtAddress, ltype: AT, ltype_str: str
     ) -> None:
@@ -107,6 +114,7 @@ class Annotator:
         "arraylabel": _annotcmd_arraylabel,
         "splitaddr": _annotcmd_splitaddr,
         "forceimm": _annotcmd_forceimm,
+        "banksetting": _annotcmd_banksetting,
     }
 
 
