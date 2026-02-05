aabf3ebd25f92bdb1b0c8508d23b89a7204af01b
GreaseMonkey <thematrixeatsyou+s1sms@gmail.com>
2026-02-05 22:56:31 +1300

    Added a tool to compute the length of the RNG sequence and to see if it loops perfectly
    It does, the period is 12573, and all things considered it doesn't seem to have anything particularly crap about it at least at first glance.

---
+++ tools/rng_seq.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+
+g_random_seed = 0
+
+
+def main() -> None:
+    did_encounter: set[int] = set()
+    chain: list[int] = []
+
+    while True:
+        v = random_A()
+        if v in did_encounter:
+            break
+        else:
+            chain.append(v)
+            did_encounter.add(v)
+
+    print(len(did_encounter), chain.index(v))
+    print("".join(f"{(v>>8)&0x1:1X}" for v in chain))
+
+
+def random_A() -> int:
+    global g_random_seed
+
+    hl = g_random_seed
+    hl = (hl * 3) & 0xFFFF
+    h = (hl & 0xFF) + (hl >> 8)
+    l = (hl & 0xFF) + h
+    h &= 0xFF
+    l &= 0xFF
+    hl = (h << 8) | l
+    hl = (hl + 0x0054) & 0xFFFF
+    g_random_seed = hl
+
+    # In reality we take from the upper byte.
+    return g_random_seed
+
+
+if __name__ == "__main__":
+    main()
