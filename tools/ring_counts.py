0050e73a751b9eee80ff1731633aadcefbfd889a
GreaseMonkey <thematrixeatsyou+s1sms@gmail.com>
2026-02-08 15:02:16 +1300

    Add ring_counts.py, a tool to count the number of rings on a level

---
+++ tools/ring_counts.py
@@ -0,0 +1,111 @@
+#!/usr/bin/env python3
+from __future__ import annotations
+
+import io
+
+import struct
+import sys
+
+
+level_count = 0x25
+layout_base_offs = 0x14000
+header_base_offs = 0x15580
+
+
+def main() -> None:
+    (in_fname,) = sys.argv[1:]
+    with open(in_fname, "rb") as infp:
+        infp.seek(header_base_offs)
+        headers = [struct.unpack("<H", infp.read(2))[0] for i in range(level_count)]
+        # print(headers)
+        for li, header_offs in enumerate(headers):
+            if header_offs == 0:
+                continue
+
+            infp.seek(header_base_offs + header_offs + 1)
+            (
+                hlx,
+                hly,
+                hx0,
+                hx1,
+                hy0,
+                hy1,
+            ) = struct.unpack("<HHHHHH", infp.read(12))
+
+            # KLUDGE: Separate special stages 4 and 8 from JUN2
+            if li == 0x07:
+                hy0 += 0x80 << 5
+            elif li in {0x1F, 0x23}:
+                hy1 -= 0x80 << 5
+
+            # print(hex(hlx), hex(hly), hex(hx0), hex(hx1), hex(hy0), hex(hy1))
+
+            x0 = hx0 >> 5
+            y0 = hy0 >> 5
+            x1 = ((hx1 - 1) >> 5) + 1 + 8
+            y1 = ((hy1 - 1) >> 5) + 1 + 6
+            infp.seek(header_base_offs + header_offs + 15)
+            (
+                layout_offs,
+                layout_sz,
+            ) = struct.unpack("<HH", infp.read(4))
+            infp.seek(header_base_offs + header_offs + 30)
+            (obj_offs,) = struct.unpack("<H", infp.read(2))
+            # print(hex(layout_offs), hex(layout_sz), hex(obj_offs))
+
+            infp.seek(layout_base_offs + layout_offs)
+            layout_compressed = infp.read(layout_sz)
+            assert len(layout_compressed) == layout_sz
+            layout = []
+            prev = -1
+            with io.BytesIO(layout_compressed) as rlefp:
+                while True:
+                    bs = rlefp.read(1)
+                    if bs == b"":
+                        break
+                    v = bs[0]
+                    if v == prev:
+                        count = rlefp.read(1)[0]
+                        if count == 0:
+                            count = 0x100
+                        layout += [prev] * count
+                        prev = -1
+                    else:
+                        layout.append(v)
+                        prev = v
+
+            infp.seek(header_base_offs + obj_offs)
+            obj_count = infp.read(1)[0]
+            assert 0 <= obj_count <= 31
+            objects = [struct.unpack("<BBB", infp.read(3)) for i in range(obj_count)]
+            ring_monitors = len([1 for (t, x, y) in objects if t == 0x01])
+            ring_a = 0
+            ring_b = 0
+            ring_double = 0
+            oob_rings = 0
+            for i, t in enumerate(layout):
+                x = i % hlx
+                y = i // hlx
+                if x0 <= x <= x1 and y0 <= y <= y1:
+                    if t == 0x79:
+                        ring_a += 1
+                    elif t == 0x7A:
+                        ring_b += 1
+                    elif t == 0x7B:
+                        ring_double += 1
+                else:
+                    if t in {0x79, 0x7A, 0x7B}:
+                        oob_rings += 2 if t == 0x7B else 1
+                        if False:
+                            print(
+                                f"OOB ring {x:02X},{y:02X} ({x0:02X}->{x1:02X}, {y0:02X}->{y1:02X})"
+                            )
+
+            ring_count = (ring_monitors * 10) + (ring_double * 2) + ring_a + ring_b
+            print(
+                f"${li:02X}: rings = {ring_count:3d} ({ring_a:3d}, {ring_b:3d}, 2x{ring_double:3d}, 10x{ring_monitors:3d}) - OOB = {oob_rings:3d}"
+            )
+
+
+if __name__ == "__main__":
+    main()
