81caaa184ae8c4ad6210a8d83836cb88f273a489
GreaseMonkey <thematrixeatsyou+s1sms@gmail.com>
2026-02-14 13:15:36 +1300

    tcls1: Implement output scrolling
    Current bottleneck on the Covington is Xvesa.
    I also did a quick test to see what happens when I set the scale to 1x and it's using about 53% CPU, so this *might* actually be a feasible port.

--- port/tcls1/tcls1.tcl
+++ port/tcls1/tcls1.tcl
@@ -47,8 +47,8 @@ proc main {rompath} {
       ;
    # Create the upscaled version of the render target
    image create photo scaleimg \
-      -width [expr {$::render_lx*$::render_scale}] \
-      -height [expr {$::render_ly*$::render_scale}] \
+      -width [expr {$::scroll_lx*$::render_scale}] \
+      -height [expr {$::scroll_ly*$::render_scale}] \
       ;
 
    # Create our canvas
@@ -59,7 +59,15 @@ proc main {rompath} {
    grid .maincanvas
 
    # Put stuff on the canvas
-   set ::ci_scaleimg_00 [.maincanvas create image [expr {$::render_scale*8}] 0 -image scaleimg -anchor nw]
+   set x0 [expr {-8*$::render_scale}]
+   set y0 [expr { 0*$::render_scale}]
+   set x1 [expr {$x0+($::scroll_lx*$::render_scale)}]
+   set y1 [expr {$y0+($::scroll_ly*$::render_scale)}]
+   # It's actually a lot faster to let the canvas scroll 4 images than it is to reblit scaled output every time.
+   set ::ci_scaleimg_00 [.maincanvas create image $x0 $y0 -image scaleimg -anchor nw -tags {scaleimg}]
+   set ::ci_scaleimg_01 [.maincanvas create image $x1 $y0 -image scaleimg -anchor nw -tags {scaleimg}]
+   set ::ci_scaleimg_10 [.maincanvas create image $x0 $y1 -image scaleimg -anchor nw -tags {scaleimg}]
+   set ::ci_scaleimg_11 [.maincanvas create image $x1 $y1 -image scaleimg -anchor nw -tags {scaleimg}]
 
    # Hide and show the main window just in case we need to update stuff
    wm withdraw .
@@ -76,7 +84,23 @@ proc async_main {} {
    load_level [expr {0x00}]
 
    # Upscale the image and render it!
-   scaleimg copy mainimg -compositingrule set -zoom $::render_scale -from 8 0 256 192
+   scaleimg copy mainimg -compositingrule set -zoom $::render_scale
+
+   # Tick the game!
+   after 20 {tick_game}
+}
+
+set ::scroll_x 0
+set ::scroll_y 0
+proc tick_game {} {
+   # Scroll the image.
+   set prev_scroll_x $::scroll_x
+   set prev_scroll_y $::scroll_y
+   set ::scroll_x [expr {($::scroll_x+5) % 256}]
+   set ::scroll_y [expr {($::scroll_y+3) % 224}]
+   .maincanvas move scaleimg [expr {$::render_scale*($prev_scroll_x-$::scroll_x)}] [expr {$::render_scale*($prev_scroll_y-$::scroll_y)}]
+
+   after 20 {tick_game}
 }
 
 proc load_level {li} {
